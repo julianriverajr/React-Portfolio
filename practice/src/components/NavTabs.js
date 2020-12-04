daec68dc7471750ceaf3c5caf984b8de6b9d3251
Julian Rivera Jr <julianriverajr@gmail.com>
2020-12-04 01:10:42 -0600

    copied over my files from where i was working before

---
+++ practice/src/components/NavTabs.js
@@ -0,0 +1,38 @@
+import React from "react";
+import "./pages/style.css";
+
+function NavTabs(props) {
+  return (
+    <ul className="nav nav-tabs">
+      <li className="nav-item">
+        <a
+          href="#aboutme"
+          onClick={() => props.handlePageChange("Home")}
+          className={props.currentPage === "Home" ? "nav-link active" : "nav-link"}
+        >
+          About Me
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#portfolio"
+          onClick={() => props.handlePageChange("About")}
+          className={props.currentPage === "About" ? "nav-link active" : "nav-link"}
+        >
+          Portfolio
+        </a>
+      </li>
+      <li className="nav-item">
+        <a
+          href="#contact"
+          onClick={() => props.handlePageChange("Contact")}
+          className={props.currentPage === "Contact" ? "nav-link active" : "nav-link"}
+        >
+          Contact
+        </a>
+      </li>
+    </ul>
+  );
+}
+
+export default NavTabs;
---
+++ practice/src/components/pages/Home.js
@@ -0,0 +1,35 @@
+import React from "react";
+import "./style.css";
+// this is my about me page
+const Home = () => (
+  <div className="wrapper">
+  <div className="boxed" id="boxed">
+            <main>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <h1>
+                            about me.
+                        </h1>
+                    </div>
+                </div>
+                <div className="row">
+                    <div className="col-sm-12">
+                        <img id="myFace" alt="me" src="https://media-exp1.licdn.com/dms/image/C4D03AQFHe7-y4AAmkg/profile-displayphoto-shrink_400_400/0/1602219508047?e=1612396800&v=beta&t=xH6MCSgiGWmTE7VJKYS3c8J63t0G_xPkQopJbQ4MIhI" /> 
+                        <p>
+                            I am a bag of meat and bones. 
+                        </p>
+                        <p>
+                            I enjoy learning, I enjoy reading, and I enjoy doing nothing, too.
+                            I'm learning to code at the Coding BootCamp at UT Austin.
+                            I look forward to continuing my learning to become a Full Stack Web Developer.
+                            I'm nearly there, only about a month left, then I'll be certified.
+                        </p>
+                        <a target="_blank" href="https://drive.google.com/file/d/1Xvio1fshe1eeOOO_t_OzbLIjFcUB7QQP/view?usp=sharing">My resume</a>
+                    </div>
+                </div>
+            </main>
+        </div>
+        </div>
+);
+
+export default Home;
